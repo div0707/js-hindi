bfbafdf8f708df13141c53c43a67f88d5ab3ceb8
Divya Prakash Singh <103468381+div0707@users.noreply.github.com>
2024-11-25 10:05:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "divya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// q => true; 0 => false
+// "" => false
+// "divya" => true
+
+let someNumber = 33
+
+let srtingNumber = String(someNumber)
+console.log(srtingNumber);
+console.log(typeof srtingNumber);
